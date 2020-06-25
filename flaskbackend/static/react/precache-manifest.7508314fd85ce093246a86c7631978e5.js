90057e3a597f991bd38d5de3323022237a682d55
barbaramachado.dex@gmail.com <barbaramachado.dex@gmail.com>
2020-06-25 15:16:58 -0300

    initial commit

---
+++ flaskbackend/static/react/precache-manifest.7508314fd85ce093246a86c7631978e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9cfa981abd5004272e0ed4ea7bce25",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "0c2b9fafc9c27868350a",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/react/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2b9fafc9c27868350a",
+    "url": "/static/react/js/main.ad5d8356.chunk.js"
+  },
+  {
+    "revision": "2b35f277c2abb0edda03",
+    "url": "/static/react/js/runtime-main.d8763b2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
